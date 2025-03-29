35e37eb4c94c15c94f37f7c0433eebc24a6c7fd6
Benigno Coj <bcojs@miumg.edu.gt>
2025-03-29 03:56:20 -0600

    Actualizacion segundo ejercicio

--- Numero_Palindromo/src/main/java/com/mycompany/numero_palindromo/Numero_Palindromo.java
+++ Numero_Palindromo/src/main/java/com/mycompany/numero_palindromo/Numero_Palindromo.java
@@ -8,9 +8,30 @@ package com.mycompany.numero_palindromo;
  *
  * @author brsan
  */
+import java.util.Scanner;
 public class Numero_Palindromo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+    // Obtener la entrada del usuario
+            int numero = Scanner.nextInt();
+
+
+    int reverseNum = 0;
+    int temp = numero;
+
+    // Invertir los dígitos del número
+    while (temp != 0) {
+        int digit = temp % 10;
+        reverseNum = reverseNum * 10 + digit;
+        temp = temp / 10;
     }
-}
+
+    // Comprueba si el número original es igual al número invertido
+    if (numero == reverseNum) {
+        System.out.println(numero +" Este numero es palindromo");
+    } else {
+        System.out.println(numero +" Este numero no es palindromo");
+    }
+}}}
