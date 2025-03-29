5f8d8fac90a18d26ae345ff40b7c30a737a1036d
Benigno Coj <bcojs@miumg.edu.gt>
2025-03-29 00:23:54 -0600

    Mi primer commit

---
+++ Formula_Cuadratica/src/main/java/com/mycompany/formula_cuadratica/Formula_Cuadratica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formula_cuadratica;
+
+/**
+ *
+ * @author brsan
+ */
+public class Formula_Cuadratica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Numero_Vampiro_con_Colmillos/src/main/java/com/mycompany/numero_vampiro_con_colmillos/Numero_Vampiro_con_Colmillos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numero_vampiro_con_colmillos;
+
+/**
+ *
+ * @author brsan
+ */
+public class Numero_Vampiro_con_Colmillos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
